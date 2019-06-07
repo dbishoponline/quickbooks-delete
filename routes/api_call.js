5137571e073bc99a711c95fa3c440f5ac289da2a
Dylan Bishop <dbishoponline@gmail.com>
2019-06-06 23:35:32 -0700

    adds deleteAllTransactions adds getAllTransactions
    adds routes for deleting and getting all transactions

--- routes/api_call.js
+++ routes/api_call.js
@@ -87,4 +87,84 @@ router.get('/refresh', function (req, res) {
   })
 })
 
+/** /api_call **/
+router.get('/get_all_transactions', function (req, res) {
+  var token = tools.getToken(req.session)
+  if(!token) return res.json({error: 'Not authorized'})
+  if(!req.session.realmId) return res.json({
+    error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
+  })
+
+  // Set up API call (with OAuth2 accessToken)
+  var selectStatement = encodeURIComponent(`select * from Purchase where TotalAmt < '100.00'`)
+  var url = config.api_uri + req.session.realmId + '/query?query=' + selectStatement
+  console.log('Making API call to: ' + url)
+  var requestObj = {
+    url: url,
+    headers: {
+      'Authorization': 'Bearer ' + token.accessToken,
+      'Accept': 'application/json'
+    }
+  }
+
+  // Make API call
+  request(requestObj, function (err, response) {
+    // Check if 401 response was returned - refresh tokens if so!
+    tools.checkForUnauthorized(req, requestObj, err, response).then(function ({err, response}) {
+      if(err || response.statusCode != 200) {
+        return res.json({error: err, statusCode: response.statusCode})
+      }
+
+      // API Call was a success!
+      res.json(JSON.parse(response.body))
+    }, function (err) {
+      console.log(err)
+      return res.json(err)
+    })
+  })
+})
+
+router.post('/delete_all_transactions', function (req, res) {
+  var token = tools.getToken(req.session)
+  if(!token) return res.json({error: 'Not authorized'})
+  if(!req.session.realmId) return res.json({
+    error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
+  })
+
+  // Set up API call (with OAuth2 accessToken)
+  var url = config.api_uri + req.session.realmId + '/purchase?operation=delete'
+  console.log('Making API call to: ' + url)
+ 
+  var requestObj = {
+    url: url,
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + token.accessToken,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'SyncToken': '2',
+      'Id': '140'
+      // 'token': token.accessToken
+    })
+  }
+
+  // Make API call
+  request(requestObj, function (err, response) {
+    // Check if 401 response was returned - refresh tokens if so!
+    tools.checkForUnauthorized(req, requestObj, err, response).then(function ({err, response}) {
+      if(err || response.statusCode != 200) {
+        return res.json({error: err, statusCode: response.statusCode})
+      }
+
+      // API Call was a success!
+      res.json(JSON.parse(response.body))
+    }, function (err) {
+      console.log(err)
+      return res.json(err)
+    })
+  })
+})
+
 module.exports = router
