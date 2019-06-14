a5e9906aa88882dea26c8e7f7672c253660c557f
Dylan Bishop <dbishoponline@gmail.com>
2019-06-14 13:43:17 -0700

    adds tools tests

--- tools/tools.test.js
+++ tools/tools.test.js
@@ -4,14 +4,21 @@ var request = require('request')
 var test = require('ava')
 
 var config = require('../config.json')
+var tools = require('./tools')
 
 var csrf = new Tokens()
 
-test('foo', t => {
-	t.fail()
+test('getQueryEndpoint() will return a url string for querying Quickbooks entities', t => {
+	t.is(
+    tools.getQueryEndpoint(`https://quickbooks.api.intuit.com/v3/company/`, 123391081299374, `select * from Purchase`),
+    `https://quickbooks.api.intuit.com/v3/company/123391081299374/query?query=select%20*%20from%20Purchase`
+  )
 })
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar')
-	t.is(await bar, 'bar')
+test('getDeleteEndpoint() will return a url string for deleting Quickbooks entities', t => {
+  t.is(
+    tools.getDeleteEndpoint(`https://quickbooks.api.intuit.com/v3/company/`, 123391081299374, 'Purchase'),
+    `https://quickbooks.api.intuit.com/v3/company/123391081299374/purchase?operation=delete&minorversion=38`
+  )
 })
+
